ab94a5535bb0f8f7f3903feb05114d2ea5e0a120
roguejuan44 <elitethan@gmail.com>
2021-03-13 19:32:49 -0700

    adding functions

--- index.js
+++ index.js
@@ -7,5 +7,14 @@ express()
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
-  .get('/signIn', (req, res) => res.render('pages/home'))
+  .get('/signIn', signIn)
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+
+
+/*-----------Functions-----------*/
+function signIn(req, res) {
+  const username = req.body.username;
+  const password = req.body.password;
+  console.log("UN:" + username);
+  console.log("PW: " + password);
+}
